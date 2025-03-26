4032ae6ad20bcb710172aaff1cbed9b4ac2b7787
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-26 12:52:48 +0530

    All the ProjectFolders

---
+++ FundooNotes/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+
+namespace FundooNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+        public UsersController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //httplocal/api/Users/Reg
+        [HttpPost]
+        [Route("Reg")]
+        public IActionResult Register(RegisterModel model)
+        {
+            var check = userManager.EmailChecker(model.Email);
+            if (check)
+            {
+                return BadRequest(new ResponseModel<UserEntity> { Success = false, Message = "Registration Failed" });
+            }
+            else
+            {
+                var result = userManager.Register(model);
+                if (result != null)
+                {
+                    return Ok(new ResponseModel<UserEntity> { Success = true, Message = "Registration Successfull", Data = result });
+                }
+                return BadRequest(new ResponseModel<UserEntity> { Success = false, Message = "Registration Failed", Data = result });
+            }
+        }
+    }
+}
---
+++ FundooNotes/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerLayer.Interfaces;
+using ManagerLayer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
+
+namespace FundooNotes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<FundooDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DbConn"]));
+            services.AddTransient<IUserRepo, UserRepo>();
+            services.AddTransient<IUserManager, UserManager>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
