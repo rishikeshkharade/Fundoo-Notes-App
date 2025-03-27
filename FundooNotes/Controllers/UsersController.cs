64bd4c020e2b8528e7adf3a3049f4b56f0d639b3
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-27 17:01:31 +0530

    Forget Password and Reset Password

--- FundooNotes/Controllers/UsersController.cs
+++ FundooNotes/Controllers/UsersController.cs
@@ -1,8 +1,13 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using CommonLayer.Models;
 using ManagerLayer.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RepositoryLayer.Entity;
 
 namespace FundooNotes.Controllers
@@ -12,9 +17,11 @@ namespace FundooNotes.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserManager userManager;
-        public UsersController(IUserManager userManager)
+        private readonly IBus bus;
+        public UsersController(IUserManager userManager, IBus bus)
         {
             this.userManager = userManager;
+            this.bus = bus;
         }
 
         //httplocal/api/Users/Reg
@@ -41,7 +48,8 @@ namespace FundooNotes.Controllers
         [HttpPost]
         [Route("Login")]
 
-        public IActionResult Login(LoginModel loginmodel) {
+        public IActionResult Login(LoginModel loginmodel)
+        {
             var result = userManager.Login(loginmodel);
             if (result != null)
             {
@@ -50,5 +58,56 @@ namespace FundooNotes.Controllers
             return BadRequest(new ResponseModel<string> { Success = false, Message = "Login Failed", Data = result });
         }
 
+        [HttpPost]
+        [Route("ForgetPass")]
+        public async Task<IActionResult> ForgetPassword(string Email)
+        {
+            try
+            {
+                if (userManager.EmailChecker(Email))
+                {
+
+                    SendingMailToUser send = new SendingMailToUser();
+                    ForgetPasswordModel forgetPasswordModel = userManager.ForgetPassword(Email);
+                    send.SendingMail(forgetPasswordModel.Email, forgetPasswordModel.Token);
+                    Uri uri = new Uri("rabbitmq://localhost/FundooNotesEmailQueue");
+                    var endPoint = await bus.GetSendEndpoint(uri);
+
+                    await endPoint.Send(forgetPasswordModel);
+
+                    return Ok(new ResponseModel<string> { Success = true, Message = "Mail Sent SuccessFully" });
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<string> { Success = false, Message = "Mail is not sent" });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("ResetPassword")]
+
+        public ActionResult ResetPassword(ResetPasswordModel reset)
+        {
+            try
+            {
+                string Email = User.FindFirst("EmailId").Value;
+                if (userManager.ResetPassword(Email, reset))
+                {
+                    return Ok(new ResponseModel<bool> { Success = true, Message = "Password Changed" });
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<bool> { Success = false, Message = "Password Not Cheanged" });
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
     }
+
 }
