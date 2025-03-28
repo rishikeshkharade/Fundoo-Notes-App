97741b31550d46459e5edbc833accc5e577efd81
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-03-28 15:37:03 +0530

    Created Notes Successfully

---
+++ ManagerLayer/Services/NotesManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
+
+namespace ManagerLayer.Services
+{
+    public class NotesManager : INotesManager
+    {
+        private readonly INotesRepo notesRepo;
+        public NotesManager(INotesRepo notesRepo)
+        {
+            this.notesRepo = notesRepo;
+        }
+        public NotesEntity CreateNote(int UserId, NotesModel notesModel)
+        {
+            return notesRepo.CreateNote(UserId, notesModel);
+        }
+    }
+}
