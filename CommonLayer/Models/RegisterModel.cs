0320bda0c258639c8b6520faf5a221c5f9b3624a
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-04-03 17:26:14 +0530

    Input Validation

--- CommonLayer/Models/RegisterModel.cs
+++ CommonLayer/Models/RegisterModel.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.Models
 {
    public class RegisterModel
     {
+        [StringLength(50,MinimumLength =3, ErrorMessage = "First Name should be minimum 3 letters.")]
         public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Last Name should be minimum 3 letters.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required.")]
         public DateTime DOB { get; set; }
+
         public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one number.")]
         public string Password { get; set; }
     }
 }
--- CommonLayer/Models/UpdateNoteModel.cs
+++ CommonLayer/Models/UpdateNoteModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace CommonLayer.Models
 {
-    public class NotesModel
+    public class UpdateNoteModel
     {
         [Required(ErrorMessage = "Title is required.")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Title should be minimum 3 letters.")]
@@ -14,6 +14,11 @@ namespace CommonLayer.Models
         [Required(ErrorMessage = "Description is required.")]
         [StringLength(500, MinimumLength = 3, ErrorMessage = "Description should be minimum 3 letters.")]
         public string Description { get; set; }
-
+        public DateTime Reminder { get; set; }
+        public string Colour { get; set; }
+        public string Image { get; set; }
+        public bool IsArchive { get; set; }
+        public bool IsPin { get; set; }
+        public bool IsTrash { get; set; }
     }
 }
